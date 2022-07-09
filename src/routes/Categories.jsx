61fb9ecbe3c7222dd0d34b7676907de8e1e70f84
waqas ahmad <waqaskanju@gmail.com>
2022-07-08 18:25:45 -0700

    add button to category page

--- src/routes/Categories.jsx
+++ src/routes/Categories.jsx
@@ -1,10 +1,5 @@
 export default function Categories() {
   return (
-    <div>
-      <h1 className="go-down">This is Categories Page</h1>
-      <p>
-        For Now it is empty. Data will follow soon.
-      </p>
-    </div>
+    <button type="button" className="primary-button">Check status</button>
   );
 }
