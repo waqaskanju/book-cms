257acac378022adc7694a61bbd6a727711c20bbd
waqas ahmad <waqaskanju@gmail.com>
2022-07-08 17:11:46 -0700

    setup book tempalte

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Books from './routes/Books';
+import Categories from './routes/Categories';
+import Navigation from './components/Navigation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-/*   <React.StrictMode>
-    <App />
-  </React.StrictMode> */
+  <React.StrictMode>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
 );
---
+++ src/routes/Categories.jsx
@@ -0,0 +1,10 @@
+export default function Categories() {
+  return (
+    <div>
+      <h1 className="go-down">This is Categories Page</h1>
+      <p>
+        For Now it is empty. Data will follow soon.
+      </p>
+    </div>
+  );
+}
